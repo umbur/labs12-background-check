4612356556280cbeee65cbd778b446874270ee14
umbur <you@example.com>
2019-05-14 16:51:48 -0600

    login endpoint partially completed

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -10,7 +10,7 @@ mudule.exports = server => {
 } 
 
 // Register Endpoint here(working on it...):
-app.post('/login',
+app.post('/register',
   passport.authenticate('local'),
   function register(req, res) {
     let user = req.body;
@@ -35,37 +35,42 @@ app.post('/login',
     res.redirect('/users/' + req.user.username);
   });
 
-// Example:
-function register(req, res) {
-    // Implement user registration:
-    let user = req.body;
-    console.log('1. req.body:', user)
-    if(user.username && user.password) {
-      const hash = bcrypt.hashSync(user.password, 4)
-      user.password = hash
-      console.log('2. hashed', user)
-      db('users').insert(user).then(result => {
-        console.log('3. result:', result)
-        const [id] = result;
-        db('users').where({id}).first().then(userAdded => {
-          res.status(200).json(userAdded)
-          console.log('4. userAdded:', userAdded)
-        })
-        .catch(err => { 
+// Login Endpoint here:
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+    function login(req, res) {
+        // implement user login
+        let { username, password } = req.body;
+        db('auth').where('username', username).first().then(user => {
+          console.log('user object:', user)
+          if(user && bcrypt.compareSync(password, user.password)) {
+            //console.log('user.password:', user.password, 'password:', password)
+            const token = generateToken(user);
+            //console.log('useraftertoken:', user, token)
+            res.status(200).json({ message: `Login completed for ${user.username}`, token })
+          } else {
+            res.status(401).json({ message: 'Invalid credentioals!' });
+          } 
+        }) .catch(err => {
           console.log(err)
-          res.status(500).json({message: 'Error accessing DB'})
+          res.status(422).json({message: "Error, probably you've made a mistake"})
         })
-      })
-      .catch(err => {
-        res.status(400).json({message: "Error, probably the user already exists"})
-        console.log(err);
-      })
-    } else {
-      res.status(422).json({message: "Error, please make sure you've both username & password!"})
-    }
-  }
+      
+      }   
+  });
 
-  // Login Endpoint here:
+  // Example: 
+  app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
 
 
     
